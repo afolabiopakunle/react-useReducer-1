8a4e8eeb8469769a5c4c0c998d74b6cb22b56551
afolabiopakunle <opakunleafolabi@gmail.com>
2022-09-19 17:52:50 +0100

    paused

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useReducer, useState} from "react";
 import "./style.css";
 
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+
+}
+function reducer(todos, action) {
+    switch (action.type) {
+      case ACTIONS.ADD_TODO:
+        return [...todos, {id: Date.now(), }]
+    }
+
+}
 export default function App() {
   
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({type: ACTIONS.ADD_TODO})
+
+    setName('')
+    console.log(name)
+  }
+  const [state, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('Afoo');
+
   return (
     <div>
-      <input type="text" />
-
-    </div>
-  );
+      <form onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      </form>
+    </div> 
+  )
 }
